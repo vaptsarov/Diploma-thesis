cdbd48809b5ceed5037d0abc346bc680cbc41a07
vaptsarov <b.vaptsarov@gmail.com>
2015-08-23 10:37:17 +0300

    #2 Init EntityFramework registration - Adding User.cs (not final)

---
+++ TestCaseManager.DB/Code First/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCaseManager.DB.Code_First
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+    }
+}
