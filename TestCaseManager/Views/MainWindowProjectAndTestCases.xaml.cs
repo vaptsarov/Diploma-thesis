6683490018b32ead42846c0ea76e8805eed4299d
Borislav Vapsarov <b.vaptsarov@gmail.com>
2015-12-22 13:12:35 +0200

    #8 Created list which will be populated from given XML file retrieved from the database containing steps and expected results for each test case.

--- TestCaseManager/Views/MainWindowProjectAndTestCases.xaml.cs
+++ TestCaseManager/Views/MainWindowProjectAndTestCases.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using FirstFloor.ModernUI.Presentation;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,8 +19,10 @@ namespace TestCaseManager.Pages
             this.SetCurrentAccentColor();
 
             AppearanceManager.Current.PropertyChanged += OnAppearanceManagerPropertyChanged;
+
+            this.listBoxStations.ItemsSource = new StepDefinitionCollection().stepDefinitionCollection;
         }
-        
+
         private void tvEmps_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             object current;
@@ -49,6 +53,40 @@ namespace TestCaseManager.Pages
         {
             this.ProjectsBorder.BorderBrush = new SolidColorBrush(AppearanceManager.Current.AccentColor);
             this.TestCasePanelBorder.BorderBrush = new SolidColorBrush(AppearanceManager.Current.AccentColor);
+            this.TestCaseEditViewBorder.BorderBrush = new SolidColorBrush(AppearanceManager.Current.AccentColor);
+        }
+    }
+
+    public sealed class StepDefinition
+    {
+        public string Step { get; set; }
+        public string ExpectedResult { get; set; }
+    }
+
+    public sealed class StepDefinitionCollection
+    {
+        public ObservableCollection<StepDefinition> stepDefinitionCollection { get; private set; }
+        public StepDefinitionCollection()
+        {
+            stepDefinitionCollection = new ObservableCollection<StepDefinition>();
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
+            stepDefinitionCollection.Add(new StepDefinition { Step = "One", ExpectedResult = "15" });
         }
     }
 }
