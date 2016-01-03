cf1c3ef4e097b00d07f946906bad963ef39e1c35
vaptsarov <vaptsarov@telerik.com>
2016-01-03 21:46:30 +0200

    #13 Area.cs was not check in.

---
+++ TestCaseManager.Core/Proxy/AreaProxy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TestCaseManager.Core.Proxy
+{
+    public class AreaProxy
+    {
+        public AreaProxy()
+        {
+            TestCasesList = new List<TestCaseProxy>();
+        }
+
+        public string Name { get; set; }
+
+        public List<TestCaseProxy> TestCasesList { get; set; }
+    }
+}
